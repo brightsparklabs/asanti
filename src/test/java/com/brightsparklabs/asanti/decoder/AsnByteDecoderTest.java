168fa48caa5eda97ed471276aba0be7d0f57b43b
Al Jeyapal <al.jeyapal@brightsparklabs.com>
2015-04-01 13:30:43 +1100

    ASN-8: Create ByteDecoderTest class skeleton

---
+++ src/test/java/com/brightsparklabs/asanti/decoder/AsnByteDecoderTest.java
@@ -0,0 +1,202 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+package com.brightsparklabs.asanti.decoder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Units tests for {@link AsnByteDecoder}
+ *
+ * @author brightSPARK Labs
+ */
+public class AsnByteDecoderTest
+{
+    // -------------------------------------------------------------------------
+    // TESTS
+    // -------------------------------------------------------------------------
+
+    @Test
+    public void testDecodeAsBitString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsBmpString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsBoolean() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsCharacterString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsDate() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsDateTime() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsDuration() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsEmbeddedPDV() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsExternal() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsGeneralizedTime() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsGeneralString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsGraphicString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsIa5String() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsIso646String() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsInteger() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsNull() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsNumericString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsOid() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsOidIri() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsPrintableString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsReal() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsRelativeOid() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsRelativeOidIri() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsTeletexString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsTime() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsTimeOfDay() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsUniversalString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsUtf8String() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsVideotexString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testDecodeAsVisibleString() throws Exception
+    {
+
+    }
+}
