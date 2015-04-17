5e56de00cbb26c9f3cfaaa819aa12d3b2f1d5501
Al Jeyapal <al.jeyapal@brightsparklabs.com>
2015-04-17 11:40:18 +1000

    ASN-93: Add OctetString validator

---
+++ src/test/java/com/brightsparklabs/asanti/validator/builtin/OctetStringValidatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+package com.brightsparklabs.asanti.validator.builtin;
+
+import com.brightsparklabs.asanti.validator.FailureType;
+import com.brightsparklabs.asanti.validator.failure.ByteValidationFailure;
+import com.google.common.collect.ImmutableSet;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Units tests for {@link OctetStringValidator}
+ *
+ * @author brightSPARK Labs
+ */
+public class OctetStringValidatorTest
+{
+    // -------------------------------------------------------------------------
+    // FIXTURES
+    // -------------------------------------------------------------------------
+
+    /** instance under test */
+    private static final OctetStringValidator instance = OctetStringValidator.getInstance();
+
+    // -------------------------------------------------------------------------
+    // TESTS
+    // -------------------------------------------------------------------------
+
+    @Test
+    public void testValidateTag() throws Exception
+    {
+        // TODO: ASN-105 need to mock DecodedAsnData
+    }
+
+    @Test
+    public void testValidateBytes() throws Exception
+    {
+        // test valid
+        byte[] bytes = new byte[] { 0x00 };
+        assertEquals(0, instance.validate(bytes).size());
+
+        // test empty
+        bytes = new byte[0];
+        assertEquals(0, instance.validate(bytes).size());
+
+        // test null
+        final ImmutableSet<ByteValidationFailure> failures = instance.validate(null);
+        assertEquals(1, failures.size());
+        final ByteValidationFailure failure = failures.iterator().next();
+        assertEquals(FailureType.DataMissing, failure.getFailureType());
+        assertEquals("No data present", failure.getFailureReason());
+    }
+}
