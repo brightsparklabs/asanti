43c57ad3ea72410fcf80eb88f51ec3afb65ab594
Michael Forman <michael.forman@brightsparklabs.com>
2015-07-17 09:06:03 +0000

    ASN-115: added a package-info file

---
+++ src/main/java/com/brightsparklabs/asanti/model/schema/tag/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+/**
+ * This package contains classes for modeling the Tag concept.
+ *
+ * @author brightSPARK Labs
+ */
+package com.brightsparklabs.asanti.model.schema.tag;
