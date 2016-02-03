fa8aa1ead5fcff01bc2bb86a51cc4e0c6393f043
Michael Forman <michael.forman@brightsparklabs.com>
2016-02-03 05:22:20 +0000

    ASN-108: added missing package-info files

---
+++ src/main/java/com/brightsparklabs/asanti/reader/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+/**
+ * This package contains all classes used for reading in ASN.1 schema and data files/data sources.
+ *
+ * @author brightSPARK Labs
+ */
+package com.brightsparklabs.asanti.reader;
---
+++ src/main/java/com/brightsparklabs/asanti/validator/failure/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+/**
+ * This package contains all classes used for modelling validation failures.
+ *
+ * @author brightSPARK Labs
+ */
+package com.brightsparklabs.asanti.validator.failure;
